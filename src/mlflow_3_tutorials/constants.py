76727e80612882060b9162fd90ca4747dbad1a29
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-27 14:37:47 +0200

    update constants.py

--- src/mlflow_3_tutorials/constants.py
+++ src/mlflow_3_tutorials/constants.py
@@ -2,11 +2,15 @@ import logging
 from typing import Final
 
 # URL and port for the MLflow tracking server.
-_URL: Final[str] = "http://localhost"
-_PORT: Final[int] = 5000
+TRACKING_URL: Final[str] = "http://localhost"
+TRACKING_PORT: Final[int] = 5000
 
 # The tracking URI for MLflow, which is the URL and port where the MLflow server is running.
-TRACKING_URI: Final[str] = f"{_URL}:{_PORT}"
+TRACKING_URI: Final[str] = f"{TRACKING_URL}:{TRACKING_PORT}"
 
 # Log level
 LOG_LEVEL: Final[int] = logging.INFO
+
+# Mlflow server configuration
+SERVER_ADDRESS: Final[str] = "127.0.0.1"
+SERVER_PORT: Final[int] = 8080
