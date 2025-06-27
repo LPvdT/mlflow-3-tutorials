11a45e7b38fce33fe00c809de560a3156117468d
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-27 14:12:25 +0200

    update server.py

--- src/mlflow_3_tutorials/lib/server.py
+++ src/mlflow_3_tutorials/lib/server.py
@@ -1,7 +1,9 @@
 import mlflow
+from loguru import logger
 
-from ..constants import TRACKING_URI
+from mlflow_3_tutorials.constants import TRACKING_URI
 
 
-def configure_tracking_server():
+def configure_tracking_server() -> None:
+    logger.info(f"Setting MLflow tracking URI to: {TRACKING_URI}")
     mlflow.set_tracking_uri(TRACKING_URI)
