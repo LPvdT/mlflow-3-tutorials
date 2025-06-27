e840159d8bf60025216c05f89df6e5b632b6d4e0
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-06-27 14:17:44 +0200

    update server.py

--- src/mlflow_3_tutorials/lib/server.py
+++ src/mlflow_3_tutorials/lib/server.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import mlflow
 from loguru import logger
 
@@ -7,3 +9,13 @@ from mlflow_3_tutorials.constants import TRACKING_URI
 def configure_tracking_server() -> None:
     logger.info(f"Setting MLflow tracking URI to: {TRACKING_URI}")
     mlflow.set_tracking_uri(TRACKING_URI)
+
+
+def start_tracking_server() -> None:
+    logger.info("Starting MLflow tracking server...")
+    host = TRACKING_URI.split(":")[0]
+    port = TRACKING_URI.split(":")[1]
+    subprocess.run(
+        ["mlflow", "server", "--host", host, "--port", port],
+        check=True,
+    )
