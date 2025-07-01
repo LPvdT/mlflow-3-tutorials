209c2515af5307b0933c16f9ce4eb510c71bd5fc
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-01 15:55:34 +0200

    feat: create wine_api_call.py

---
+++ src/mlflow_3_tutorials/wine_api_call.py
@@ -0,0 +1,40 @@
+import json
+
+import requests
+from loguru import logger
+
+from mlflow_3_tutorials.lib.utils import as_json
+
+# Prepare test data
+test_wine = {
+    "dataframe_split": {
+        "columns": [
+            "fixed acidity",
+            "volatile acidity",
+            "citric acid",
+            "residual sugar",
+            "chlorides",
+            "free sulfur dioxide",
+            "total sulfur dioxide",
+            "density",
+            "pH",
+            "sulphates",
+            "alcohol",
+        ],
+        "data": [
+            [7.0, 0.27, 0.36, 20.7, 0.045, 45, 170, 1.001, 3.0, 0.45, 8.8]
+        ],
+    }
+}
+
+# Make prediction request
+response = requests.post(
+    "http://localhost:5002/invocations",
+    headers={"Content-Type": "application/json"},
+    data=json.dumps(test_wine),
+    timeout=500,
+)
+
+
+prediction = response.json()
+logger.info(f"Predicted wine quality: {as_json(prediction['predictions'][0])}")
