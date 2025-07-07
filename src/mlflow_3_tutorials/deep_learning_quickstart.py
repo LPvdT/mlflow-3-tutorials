f66fedc45abb205292e75eb041060c499a765ef6
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-07 12:55:10 +0200

    update deep_learning_quickstart.py and dl_utils.py

--- src/mlflow_3_tutorials/deep_learning_quickstart.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-import mlflow.pytorch
-import pandas as pd
-import torch
-import torch.nn as nn
-from mlflow.entities import Dataset
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
