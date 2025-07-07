d2d8b04a8bb81f509811c448f2cbdc9a7f1de41f
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-07 13:19:49 +0200

    update deep_learning_quickstart.py

--- src/mlflow_3_tutorials/deep_learning_quickstart.py
+++ src/mlflow_3_tutorials/deep_learning_quickstart.py
@@ -1,6 +1,15 @@
+from typing import cast
+
+import mlflow
+import mlflow.pytorch
 import pandas as pd
+import torch
+from mlflow.data import pandas_dataset
+from mlflow.entities import Dataset
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+
+from mlflow_3_tutorials.lib.dl_utils import IrisClassifier, prepare_data
 
 
 def main() -> None: ...
@@ -13,7 +22,20 @@ iris_df, iris_target = (
     iris.target,  # type: ignore
 )
 
-# Split the dataset into training and test sets
-train_data, test_data, train_labels, test_labels = train_test_split(
-    iris_df, iris_target, test_size=0.2, random_state=42
-)
+# Split into training and testing datasets
+train_df, test_df = train_test_split(iris_df, test_size=0.2, random_state=42)
+train_df = cast("pd.DataFrame", train_df)
+test_df = cast("pd.DataFrame", test_df)
+
+# Prepare training data
+train_dataset = pandas_dataset.from_pandas(train_df, name="train")
+X_train, y_train = prepare_data(train_dataset.df)
+
+# Define the PyTorch model and move it to the device
+input_size = X_train.shape[1]
+hidden_size = 16
+output_size = len(iris.target_name)  # type: ignore
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+scripted_model = IrisClassifier(input_size, hidden_size, output_size).to(device)
+scripted_model = torch.jit.script(scripted_model)
