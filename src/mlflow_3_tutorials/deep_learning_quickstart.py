04ef25f5126c5cf4f007c21ea24e973b600a4384
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-07 12:46:25 +0200

    feat: create deep_learning_quickstart.py

---
+++ src/mlflow_3_tutorials/deep_learning_quickstart.py
@@ -0,0 +1,8 @@
+import mlflow
+import mlflow.pytorch
+import pandas as pd
+import torch
+import torch.nn as nn
+from mlflow.entities import Dataset
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
