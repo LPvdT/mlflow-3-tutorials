82cc54ec1d942c7015b72e4dfe906f3106878178
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-07 14:30:36 +0200

    update constants.py

--- src/mlflow_3_tutorials/lib/constants.py
+++ src/mlflow_3_tutorials/lib/constants.py
@@ -8,7 +8,7 @@ WINE_QUALITY_DATA_URL: Final[str] = (
 
 # URL and port for the MLflow tracking server.
 TRACKING_URL: Final[str] = "http://127.0.0.1"
-TRACKING_PORT: Final[int] = 8080
+TRACKING_PORT: Final[int] = 5000
 
 # The tracking URI for MLflow, which is the URL and port where the MLflow server is running.
 TRACKING_URI: Final[str] = f"{TRACKING_URL}:{TRACKING_PORT}"
@@ -18,4 +18,4 @@ LOG_LEVEL: Final[int] = logging.INFO
 
 # Mlflow server configuration
 SERVER_ADDRESS: Final[str] = "127.0.0.1"
-SERVER_PORT: Final[int] = 8080
+SERVER_PORT: Final[int] = 5000
