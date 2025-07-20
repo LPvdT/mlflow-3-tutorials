ca9279a13f1bd3820d284f2803b81931de006a74
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-20 12:19:14 +0200

    update utils.py

--- src/mlflow_3_tutorials/lib/utils.py
+++ src/mlflow_3_tutorials/lib/utils.py
@@ -2,10 +2,13 @@ import json
 from datetime import datetime
 from typing import Any
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pytz
+import seaborn as sns
 from loguru import logger
+from matplotlib.figure import Figure
 
 
 def as_json(obj: object, *, indent: int = 2) -> str:
@@ -179,3 +182,52 @@ def generate_apple_sales_data_with_promo_adjustment(
             "stock_available",
         ]
     )
+
+
+def plot_correlation_with_demand(
+    df: pd.DataFrame, save_path: str | None = None
+) -> Figure:
+    """
+    Plots the correlation of each variable in the dataframe with the 'demand' column.
+
+    Args:
+    - df (pd.DataFrame): DataFrame containing the data, including a 'demand' column.
+    - save_path (str, optional): Path to save the generated plot. If not specified, plot won't be saved.
+
+    Returns:
+    - None (Displays the plot on a Jupyter window)
+    """
+
+    # Compute correlations between all variables and 'demand'
+    correlations = df.corr()["demand"].drop("demand").sort_values()  # type: ignore
+
+    # Generate a color palette from red to green
+    colors = sns.diverging_palette(10, 130, as_cmap=True)
+    color_mapped = correlations.map(colors)
+
+    # Set Seaborn style
+    sns.set_style(
+        "whitegrid", {"axes.facecolor": "#c2c4c2", "grid.linewidth": 1.5}
+    )  # Light grey background and thicker grid lines
+
+    # Create bar plot
+    fig = plt.figure(figsize=(12, 8))
+    plt.barh(correlations.index, correlations.values, color=color_mapped)  # type: ignore
+
+    # Set labels and title with increased font size
+    plt.title("Correlation with Demand", fontsize=18)
+    plt.xlabel("Correlation Coefficient", fontsize=16)
+    plt.ylabel("Variable", fontsize=16)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.grid(axis="x")
+
+    plt.tight_layout()
+
+    # Save the plot if save_path is specified
+    if save_path:
+        plt.savefig(save_path, format="png", dpi=600)
+
+    plt.close(fig)
+
+    return fig
