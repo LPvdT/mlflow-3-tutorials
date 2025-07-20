a090cc2f11a446022aad9fd6d5a4470a38c8543b
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-07-20 12:29:46 +0200

    update utils.py

--- src/mlflow_3_tutorials/lib/utils.py
+++ src/mlflow_3_tutorials/lib/utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Any
 
 import matplotlib.pyplot as plt
+import mlflow
 import numpy as np
 import pandas as pd
 import pytz
@@ -313,3 +314,23 @@ def plot_feature_importance(
     plt.close(fig)
 
     return fig
+
+
+def get_or_create_experiment(experiment_name: str) -> str:
+    """
+    Retrieve the ID of an existing MLflow experiment or create a new one if it doesn't exist.
+
+    This function checks if an experiment with the given name exists within MLflow.
+    If it does, the function returns its ID. If not, it creates a new experiment
+    with the provided name and returns its ID.
+
+    Parameters:
+        experiment_name (str): Name of the MLflow experiment.
+
+    Returns:
+        str: ID of the existing or newly created MLflow experiment.
+    """
+
+    if experiment := mlflow.get_experiment_by_name(experiment_name):
+        return experiment.experiment_id
+    return mlflow.create_experiment(experiment_name)
